a42c219563a28e8c7f2ca6c101cc3a6bd07c20b9
matteotaf <tafuromatteo00@gmail.com>
2022-01-21 19:34:37 +0100

    Removed unused file from data_utils, datasets, defenses

--- Fairness_attack/run_attacks.py
+++ Fairness_attack/run_attacks.py
@@ -1,16 +1,11 @@
 import os
 
+os.system('coverage erase')
 # IAF
-os.system('coverage run run_gradient_em_attack.py --em_iter 0 --total_grad_iter 10000 --dataset german --use_slab --epsilon 0.5 --method IAF --sensitive_feature_idx 36 --sensitive_attr_filename german_group_label.npz')
-# os.system('coverage html --title=IAF_attack')
-# os.system("mv '/Users/matteo/Dropbox/Università/Module 3/FACT/Project/FACT-AI/Fairness_attack/htmlcov' '/Users/matteo/Dropbox/Università/Module 3/FACT/Project/FACT-AI/Fairness_attack/IAF'")
-
+os.system('coverage run -a run_gradient_em_attack.py --em_iter 0 --total_grad_iter 10000 --dataset german --use_slab --epsilon 0.5 --method IAF --sensitive_feature_idx 36 --sensitive_attr_filename german_group_label.npz')
 # RAA
-os.system('coverage run run_gradient_em_attack.py --em_iter 0 --total_grad_iter 10000 --dataset german --use_slab --epsilon 0.5 --method IAF --sensitive_feature_idx 36 --sensitive_attr_filename german_group_label.npz')
-# os.system('coverage html --title=RAA_attack')
-# os.system("mv '/Users/matteo/Dropbox/Università/Module 3/FACT/Project/FACT-AI/Fairness_attack/htmlcov' '/Users/matteo/Dropbox/Università/Module 3/FACT/Project/FACT-AI/Fairness_attack/RAA'")
-
+os.system('coverage run -a run_gradient_em_attack.py --em_iter 0 --total_grad_iter 10000 --dataset german --use_slab --epsilon 0.5 --method IAF --sensitive_feature_idx 36 --sensitive_attr_filename german_group_label.npz')
 # NRAA
-os.system('coverage run run_gradient_em_attack.py --em_iter 0 --total_grad_iter 10000 --dataset german --use_slab --epsilon 0.5 --method IAF --sensitive_feature_idx 36 --sensitive_attr_filename german_group_label.npz')
-# os.system('coverage html --title=NRAA_attack')
-# os.system("mv '/Users/matteo/Dropbox/Università/Module 3/FACT/Project/FACT-AI/Fairness_attack/htmlcov' '/Users/matteo/Dropbox/Università/Module 3/FACT/Project/FACT-AI/Fairness_attack/NRAA'")
+os.system('coverage run -a run_gradient_em_attack.py --em_iter 0 --total_grad_iter 10000 --dataset german --use_slab --epsilon 0.5 --method IAF --sensitive_feature_idx 36 --sensitive_attr_filename german_group_label.npz')
+
+os.system('coverage html --title=Total_coverage')
