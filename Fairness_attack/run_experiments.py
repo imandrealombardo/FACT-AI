4c98bab2f3e6c33095d24644c10c62741e7f9fe7
matteotaf <tafuromatteo00@gmail.com>
2022-01-26 18:52:16 +0100

    Fixed run_experiments.py to do the correct runs

--- Fairness_attack/run_experiments.py
+++ Fairness_attack/run_experiments.py
@@ -1,22 +1,39 @@
+
 import os
 
-epsilons = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
-lambdas =  [0.8, 0.9, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5]
-datasets = ["compas"]                # ["german", "compas", "drug"]
-attacks =  ["IAF"]                   # ["IAF", "RAA", "RNAA", "Koh", "Solans"]
-stopping_methods = ["Parity"]        # ["Parity", "Accuracy"]
+datasets = ["compas", "german", "drug"]
+attacks =  ["IAF", "RAA", "RNAA", "Koh", "Solans"]
+stopping_methods = ["Parity", "Accuracy"]
 
 
-for eps in epsilons: 
-    for lamb in lambdas: 
-        for dataset in datasets:
-            for attack in attacks:
-                for stopping_method in stopping_methods:
+# ================= To replicate Figure 2 of the original paper: ================= 
 
-                    # Print run information in the terminal
-                    print("\n============================================")
-                    print(f"Run parameters: \n - Dataset: {dataset}\n - Attack: {attack}\n - Stopping method: {stopping_method}\n - Epsilon: {eps}, Lambda: {lamb}")
-                    print("============================================")
-                    
-                    os.system(f"python -u run_gradient_em_attack.py --total_grad_iter 10000 --dataset {dataset} --use_slab --epsilon {eps} --lamb {lamb} --method {attack} --sensitive_attr_filename {dataset}_group_label.npz --stopping_method {stopping_methods} --stop_after 5\n")
+lamb = 1
+epsilon = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
 
+for dataset in datasets:
+    for attack in attacks:
+        for stopping_method in stopping_methods:
+            for eps in epsilon:
+                print("\n============================================")
+                print("Reproducing the results of Figure 2...")
+                print(f"Run parameters: \n - Dataset: {dataset}\n - Attack: {attack}\n - Stopping method: {stopping_method}\n - Epsilon: {eps}, Lambda: {lamb}")
+                print("============================================")
+                
+                os.system(f"python -u run_gradient_em_attack.py --total_grad_iter 10000 --dataset {dataset} --use_slab --epsilon {eps} --lamb {lamb} --method {attack} --sensitive_attr_filename {dataset}_group_label.npz --stopping_method {stopping_methods} --stop_after 5\n")
+
+# ================= To replicate Figure 3 of the original paper: =================
+
+lambdas = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0]
+epsilon = [0.1, 0.5, 1.0]
+
+for dataset in datasets:
+    for attack in attacks:
+        for stopping_method in stopping_methods:
+            for eps in epsilon:
+                print("\n============================================")
+                print("Reproducing the results of Figure 3...")
+                print(f"Run parameters: \n - Dataset: {dataset}\n - Attack: {attack}\n - Stopping method: {stopping_method}\n - Epsilon: {eps}, Lambda: {lamb}")
+                print("============================================")
+                
+                os.system(f"python -u run_gradient_em_attack.py --total_grad_iter 10000 --dataset {dataset} --use_slab --epsilon {eps} --lamb {lamb} --method {attack} --sensitive_attr_filename {dataset}_group_label.npz --stopping_method {stopping_methods} --stop_after 5\n")
