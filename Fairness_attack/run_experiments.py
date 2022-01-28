db7376e00fbd137101407bc118b339166f1cb6b5
matteotaf <tafuromatteo00@gmail.com>
2022-01-28 19:57:14 +0100

    Fixed german checkpoints

--- Fairness_attack/run_experiments.py
+++ Fairness_attack/run_experiments.py
@@ -1,24 +1,25 @@
 import os
 
-datasets = ["drug"]
-attacks =  ["Solans", "IAF"]
-stopping_methods = ["Accuracy"]
+datasets = ["german"]
+attacks =  ["IAF"]
+stopping_methods = ["Accuracy", "Parity"]
 
 # ================= To replicate Figure 2 of the original paper: ================= 
 
-lamb = 1
-epsilon = [0.5]
+lambdas = [0.9, 1.0]
+epsilon = [0.1, 0.5, 1.0]
 
 for dataset in datasets:
     for attack in attacks:
         for stopping_method in stopping_methods:
             for eps in epsilon:
-                print("\n============================================")
-                print("Reproducing the results of Figure 2...")
-                print(f"Run parameters: \n - Dataset: {dataset}\n - Attack: {attack}\n - Stopping method: {stopping_method}\n - Epsilon: {eps}, Lambda: {lamb}")
-                print("============================================")
-                
-                os.system(f"python -u run_gradient_em_attack.py --total_grad_iter 10000 --dataset {dataset} --use_slab --epsilon {eps} --lamb {lamb} --method {attack} --stopping_method {stopping_method} --log_metrics True --stop_after 4\n")
+                for lamb in lambdas:
+                    print("\n============================================")
+                    print("Reproducing the results of Figure 2...")
+                    print(f"Run parameters: \n - Dataset: {dataset}\n - Attack: {attack}\n - Stopping method: {stopping_method}\n - Epsilon: {eps}, Lambda: {lamb}")
+                    print("============================================")
+                    
+                    os.system(f"python -u run_gradient_em_attack.py --total_grad_iter 10000 --dataset {dataset} --use_slab --epsilon {eps} --lamb {lamb} --method {attack} --stopping_method {stopping_method} --stop_after 2\n")
 
 # # ================= To replicate Figure 3 of the original paper: =================
 
